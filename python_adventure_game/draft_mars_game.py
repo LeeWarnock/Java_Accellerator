b6f64cba5fecc76bd5dc66341ec607faf82b85a5
Lee Warnock <clwarnock@gmail.com>
2022-10-27 20:33:04 -0500

    add draft_mars and start setup

---
+++ python_adventure_game/draft_mars_game.py
@@ -0,0 +1,11 @@
+import random
+import time
+
+choose = ["y","n"]
+
+def print_line():
+    print(narration)
+    time.sleep(1)
+
+def intro():
+    directions = ["forward", "backward", "left", "right"]
