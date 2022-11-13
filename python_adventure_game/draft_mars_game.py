cbfda38cc726ab040c6b29ccad29addf673e618c
Lee Warnock <clwarnock@gmail.com>
2022-11-13 16:29:40 -0600

    add definition sections

--- python_adventure_game/draft_mars_game.py
+++ python_adventure_game/draft_mars_game.py
@@ -1,11 +1,30 @@
+#Import Modules
 import random
 import time
 
+#create choices and directions
 choose = ["y","n"]
+directions = ["forward", "backward", "left", "right"]
 
+#random.choice definition
+
+#random.randint definition
+
+
+#print line function with sleep
 def print_line():
     print(narration)
     time.sleep(1)
 
-def intro():
-    directions = ["forward", "backward", "left", "right"]
+
+
+#intro function
+def introduction():
+
+#define surface
+
+#define Mirage
+
+#define Martian Temple
+
+#define reactor
