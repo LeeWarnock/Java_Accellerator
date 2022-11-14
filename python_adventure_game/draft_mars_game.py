3176eca68d3568e7fd060e5524c28572f32a507a
Lee Warnock <clwarnock@gmail.com>
2022-11-13 18:56:02 -0600

    add restart game dialog

--- python_adventure_game/draft_mars_game.py
+++ python_adventure_game/draft_mars_game.py
@@ -16,8 +16,6 @@ def print_line():
     print(narration)
     time.sleep(1)
 
-
-
 #intro function
 def introduction():
 
@@ -28,3 +26,18 @@ def introduction():
 #define Martian Temple
 
 #define reactor
+
+#define asphyxiation
+
+#define play again
+def restart_game():
+    restart = input("GAME OVER./nWould you like to play again? (y/n)")
+    if restart == "y":
+        print_line("You close your eyes as a slight dizziness washes over you. A sensation of falling jolts you awake and you open your eyes to find yourself sitting at the controls of your shuttle. Was it all a dream?")
+        start_game()
+    elif restart == "n":
+        print_line("You close your eyes as you accept your fate. With each breath the air in your suit becomes more acrid. As your consciousness slips away a wave of euphoric understanding washes over you, and your essence becomes one with the universe.")
+
+#start the game function
+def start_game():
+start_game()
